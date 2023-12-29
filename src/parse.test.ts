e20fa30709616dfb45e214ec3bbf365ba08051cc
Ivo <ivowijnker@hotmail.com>
2023-12-29 12:48:38 -0500

    simple parse tests

---
+++ src/parse.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, test } from "vitest"
+import { GraphData } from "./evaluate"
+import { Log, getGraphDataFromParseResult, parse } from "./parse"
+
+// Feb 02 2000
+const startDate = new Date(2000, 1, 2)
+
+const nrDays = 3
+
+describe("parse", () => {
+  test("success", () => {
+    const code = `
+      var a = 1
+
+      date b = */*/*
+
+      at b, a += 1
+    `
+
+    const parseResult = parse(code)
+    const graphData = getGraphDataFromParseResult(parseResult, startDate, nrDays)
+    const expected: GraphData = {
+      dataPerStateKey: [
+        {
+          stateKey: "a",
+          valuePerDay: [2, 3, 4],
+        },
+      ],
+      range: [0, 4],
+    }
+    expect(graphData).toEqual(expected)
+  })
+
+  test("parse error", () => {
+    const code = `abc`
+    const parseResult = parse(code)
+    const expected: Log = [
+      {
+        line: 1,
+        charPositionInLine: 1,
+        msg: "extraneous input 'abc' expecting <EOF>",
+      },
+    ]
+    expect(parseResult.log).toEqual(expected)
+  })
+
+  test("semantic error", () => {
+    const code = `
+      var a = 1
+      var a = 1
+    `
+    const parseResult = parse(code)
+    const expected: Log = [
+      {
+        line: 3,
+        charPositionInLine: 11,
+        msg: "name already exists",
+      },
+    ]
+    expect(parseResult.log).toEqual(expected)
+  })
+})
