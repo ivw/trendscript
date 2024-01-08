0803459aeed9e5314a73a326145b37b1c800f7ad
Ivo <ivowijnker@hotmail.com>
2024-01-08 17:22:23 -0500

    use localStorage

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,22 @@ import { GraphData } from "./evaluate"
 import { parse } from "./parse"
 import "./style.css"
 
+const localStorageKey = "input"
+
 const codeInput = document.getElementById("code-input") as HTMLTextAreaElement
-codeInput.value = defaultInput
+codeInput.value = getInitialInput()
 
 let graphData: GraphData | null = null
 
 function updateGraphData() {
-  const parseResult = parse(codeInput.value)
+  const code = codeInput.value
+  const parseResult = parse(code)
   displayMessages(parseResult.log)
   if (parseResult.graphData) {
     graphData = parseResult.graphData
     render(graphData)
   }
+  localStorage.setItem(localStorageKey, code)
 }
 
 updateGraphData()
@@ -32,3 +36,13 @@ window.addEventListener(
     }
   }, 50),
 )
+
+function getInitialInput(): string {
+  try {
+    const storedCode = localStorage.getItem(localStorageKey)
+    if (storedCode) {
+      return storedCode
+    }
+  } catch (e) {}
+  return defaultInput
+}
